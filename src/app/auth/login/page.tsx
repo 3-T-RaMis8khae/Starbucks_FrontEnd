3a11a5812df7b7f8f90608b49f3eff8be4fb4ff0
ParkSinGyu <singyu13@gmail.com>
2024-08-28 12:25:02 +0900

    - FEA/develop: install chadcn library and reset login page.tsx
    -- Body
    ---- (Why): []
    ---- Done:
    ---- Todo:
    ---- Note:
    
    -- Resolves: []
    -- See also: []

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,15 +1,7 @@
-import CloseIcon from "@/assets/svg/close.svg"
-import CloseURI from "@/assets/svg/close.svg?url"
-
-import Image from "next/image"
 
 export default function LoginPage() {
 	return (
 		<main>
-			<CloseIcon></CloseIcon>
-			<CloseIcon width={"50px"} height={"50px"}></CloseIcon>
-			<Image width={50} height={50} src={CloseURI} alt={"close"}></Image>
-			<Image src={CloseURI} alt={"close"}></Image>
 		</main>
 	)
 }
