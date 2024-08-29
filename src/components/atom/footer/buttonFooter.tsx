cded5073faa2d39ed3343c3a631b757368b051de
ParkSinGyu <singyu13@gmail.com>
2024-08-29 13:12:02 +0900

    - FEA/develop: create buttonFooter.tsx, modify button.tsx
    ---- (Why): []
    ---- Done:
    ---- Todo:
    ---- Note:
    
    -- Resolves: []
    -- See also: []

---
+++ src/components/atom/footer/buttonFooter.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button"
+import React from "react"
+
+interface BtFooterIf {
+	button_props?: React.InputHTMLAttributes<HTMLButtonElement>
+	button_title?: string
+}
+
+export default function ButtonFooter({ button_props, button_title }: BtFooterIf) {
+	return (
+		<footer
+			className="
+				w-full h-[90px] bg-white shadow-top shadow-[0_2px_10px_efeeed]
+				flex justify-center
+				fixed left-0 bottom-0
+				px-10
+			"
+		>
+			<Button
+				variant="sbGreen"
+				size="sbFooter"
+				className="text-white w-full mt-5"
+				{...{ props: button_props }}
+			>
+				{button_title}
+			</Button>
+		</footer>
+	)
+}
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,13 +14,15 @@ const buttonVariants = cva(
 				outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
 				secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
 				ghost: "hover:bg-accent hover:text-accent-foreground",
-				link: "text-primary underline-offset-4 hover:underline"
+				link: "text-primary underline-offset-4 hover:underline",
+				sbGreen: "bg-sb-green-100 active:opacity-80 rounded-3xl text-white"
 			},
 			size: {
 				default: "h-10 px-4 py-2",
 				sm: "h-9 rounded-md px-3",
 				lg: "h-11 rounded-md px-8",
-				icon: "h-10 w-10"
+				icon: "h-10 w-10",
+				sbFooter: "h-10"
 			}
 		},
 		defaultVariants: {
