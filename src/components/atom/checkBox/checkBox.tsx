88d64d2b15e5395e5826bb258e1acabbf7d76f1d
ParkSinGyu <singyu13@gmail.com>
2024-08-30 15:06:44 +0900

    - UPDATE/develop: Update checkBox.tsx components
    ---- (Why): []
    ---- Done:
        * add ct_container_props to manipulate container tag's props
    ---- Todo:
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

--- src/components/atom/checkBox/checkBox.tsx
+++ src/components/atom/checkBox/checkBox.tsx
@@ -13,6 +13,7 @@ type CheckBoxProps = {
 	ct_icon_alt?: string
 	ct_href?: string
 	ct_label?: string
+	ct_container_props?: React.HTMLAttributes<HTMLInputElement>
 }
 
 const CheckBox = React.forwardRef<
@@ -26,21 +27,25 @@ const CheckBox = React.forwardRef<
 			ct_icon_alt = "right-caret.svg",
 			ct_href = "/",
 			ct_label = "",
+			ct_container_props = {},
 			...props
 		},
 		ref
 	) => {
 		return (
-			<div className="w-full h-7 bg-white flex items-center justify-between">
+			<div
+				{...ct_container_props}
+				className={`w-full h-7 bg-white flex items-center justify-between ${ct_container_props.className}`}
+			>
 				<div className="flex items-center">
 					<Checkbox
-						className="w-5 h-5 border-sb-green-100 data-[state=checked]:bg-sb-green-100"
 						ref={ref}
 						{...props}
+						className={`w-5 h-5 border-sb-green-100 data-[state=checked]:bg-sb-green-100 ${props.className}`}
 					></Checkbox>
 					<label
 						htmlFor={`${props.id}`}
-						className="text-sm pl-2 select-none font-normal text-sb-black-100 "
+						className="text-sm pl-2 select-none font-normal text-sb-black-100 cursor-pointer"
 					>
 						{ct_label}
 					</label>
@@ -48,7 +53,7 @@ const CheckBox = React.forwardRef<
 				{ct_showIcon && (
 					<Link
 						href={ct_href}
-						className="w-6 h-6 mr-2 flex items-center justify-center"
+						className="w-6 h-6 mr-2 flex items-center justify-center select-none"
 					>
 						<Image
 							src={ct_icon ?? caretRightURL}
