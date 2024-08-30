51747d619f49ffaa972a38f4a3cedbb3c55a1374
ParkSinGyu <singyu13@gmail.com>
2024-08-30 16:02:47 +0900

    - UPDATE/develop: Add middleComponent prop in baseHeader.tsx
    ---- (Why): []
    ---- Done:
    ---- Todo:
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

--- src/components/atom/header/baseHeader.tsx
+++ src/components/atom/header/baseHeader.tsx
@@ -3,6 +3,7 @@ import React from "react"
 interface BaseHeaderProps {
 	leftComponent?: React.ReactNode
 	rightComponent?: React.ReactNode
+	middleComponent?: React.ReactNode
 	title?: string
 }
 
@@ -10,21 +11,25 @@ function BaseHeader(
 	props: BaseHeaderProps = {
 		leftComponent: <></>,
 		rightComponent: <></>,
+		middleComponent: undefined,
 		title: ""
 	}
 ) {
 	return (
 		<header className="w-full h-[50px] py-[10px] px-[24px] fixed flex items-center justify-between bg-white">
 			{props.leftComponent}
-			<h1
-				className="
+			{props.middleComponent ?? (
+				<h1
+					className="
 					absolute select-none m-0
 					top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]
 					font-semibold text-sb-black-100
 				"
-			>
-				{props.title}
-			</h1>
+				>
+					{props.title}
+				</h1>
+			)}
+
 			{props.rightComponent}
 		</header>
 	)
