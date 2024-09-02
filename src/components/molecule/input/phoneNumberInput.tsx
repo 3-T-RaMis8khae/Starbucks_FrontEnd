56669a4b65999e0b22500a0ab26dabe0243aa4c1
ParkSinGyu <singyu13@gmail.com>
2024-09-02 13:39:06 +0900

    - FEA/develop: Create phoneNumberInput.tsx component only for design
    ---- (Why): []
    ---- Done:
    ---- Todo:
        * implement functionality
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

---
+++ src/components/molecule/input/phoneNumberInput.tsx
@@ -0,0 +1,55 @@
+import React from "react"
+
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectTrigger,
+	SelectValue
+} from "@/components/ui/select"
+import { BaseInput } from "@/components/atom/input/baseInput"
+import { Button } from "@/components/ui/button"
+
+interface PhoneNumberInputProps {}
+
+// note : we might need to modify styles for responsive view and ux
+function PhoneNumberInput(props: PhoneNumberInputProps) {
+	return (
+		<fieldset className="flex items-center justify-between">
+			<div className="flex items-center">
+				<Select>
+					<SelectTrigger className="w-[120px] border-none text-base text-sb-gray-100 focus:border-none px-1">
+						<SelectValue
+							placeholder="통신사 선택"
+							defaultValue="SKT"
+						></SelectValue>
+					</SelectTrigger>
+					<SelectContent>
+						<SelectGroup className="*:text-base *:text-sb-gray-100 h-[100px]">
+							<SelectItem value="SKT">SKT</SelectItem>
+							<SelectItem value="KT">KT</SelectItem>
+							<SelectItem value="LGU+">LGU+</SelectItem>
+							<SelectItem value="SKT-Economical">SKT 알뜰폰</SelectItem>
+							<SelectItem value="KT-Economical">KT 알뜰폰</SelectItem>
+							<SelectItem value="LGU+-Economical">LGU+ 알뜰폰</SelectItem>
+						</SelectGroup>
+					</SelectContent>
+				</Select>
+
+				<BaseInput
+					ct_type="tel"
+					placeholder="휴대폰 번호"
+					maxLength={11}
+					className="border-none ml-1"
+				></BaseInput>
+			</div>
+
+			<Button variant="sbGreenGhost" className="text-sm font-medium">
+				인증요청
+			</Button>
+		</fieldset>
+	)
+}
+
+export default PhoneNumberInput
