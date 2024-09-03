ced98f0e20444661433e445d0071204cbbf0164a
ParkSinGyu <singyu13@gmail.com>
2024-09-03 18:03:57 +0900

    - MODIFY/develop: . ---- (Why): [] ---- Done: ---- Todo: ---- Note: []
    -- Resolves: []
    -- See also: []

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,3 +1,19 @@
 export default function SignupPage() {
-	return <main>Signup Page</main>
+	const serchParam
+	return (
+		<main>
+			<form action="">
+				{/*<agreeComp></agreeComp>*/}
+				{/*<agreeEmail></agreeEmail>*/}
+				{/*<agreeMemberAccount></agreeMemberAccount>*/}
+				{/*<agreeNickName></agreeNickName>*/}
+			</form>
+		</main>
+	)
 }
+
+/*
+주소창을 활용 
+1. 회원가입 경로에서 쿼리 파라미터를 변경함으로써 화면을 전환시킨다.
+
+ */
