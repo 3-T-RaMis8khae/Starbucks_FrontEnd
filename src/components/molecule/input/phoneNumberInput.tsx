4741eb1486a4cf9e13142ef55d1c95d690039fea
ParkSinGyu <singyu13@gmail.com>
2024-09-03 14:28:25 +0900

    - Modify/develop: Replace values to mobileCarrier values for SelectItem in phoneNumberInput.tsx component
    ---- (Why): []
    ---- Done:
    ---- Todo:
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

--- src/components/molecule/input/phoneNumberInput.tsx
+++ src/components/molecule/input/phoneNumberInput.tsx
@@ -10,6 +10,17 @@ import {
 } from "@/components/ui/select"
 import { BaseInput } from "@/components/atom/input/baseInput"
 import { Button } from "@/components/ui/button"
+import { ValueOf } from "@/lib/types"
+
+export const mobileCarrier = {
+	skt: "SKT",
+	kt: "KT",
+	lgu: "LGU+",
+	sktEc: "SKT-Economical",
+	ktEc: "KT-Economical",
+	lguEc: "LGU+-Economical"
+}
+export type MobileCarrierValueType = ValueOf<typeof mobileCarrier>
 
 interface PhoneNumberInputProps {}
 
@@ -22,23 +33,23 @@ function PhoneNumberInput(props: PhoneNumberInputProps) {
 					<SelectTrigger className="w-[120px] border-none text-base text-sb-gray-100 focus:border-none px-1">
 						<SelectValue
 							placeholder="통신사 선택"
-							defaultValue="SKT"
+							defaultValue={mobileCarrier.skt}
 						></SelectValue>
 					</SelectTrigger>
 					<SelectContent>
 						<SelectGroup className="*:text-base *:text-sb-gray-100 h-[100px]">
-							<SelectItem value="SKT">SKT</SelectItem>
-							<SelectItem value="KT">KT</SelectItem>
-							<SelectItem value="LGU+">LGU+</SelectItem>
-							<SelectItem value="SKT-Economical">SKT 알뜰폰</SelectItem>
-							<SelectItem value="KT-Economical">KT 알뜰폰</SelectItem>
-							<SelectItem value="LGU+-Economical">LGU+ 알뜰폰</SelectItem>
+							<SelectItem value={mobileCarrier.skt}>SKT</SelectItem>
+							<SelectItem value={mobileCarrier.kt}>KT</SelectItem>
+							<SelectItem value={mobileCarrier.lgu}>LGU+</SelectItem>
+							<SelectItem value={mobileCarrier.sktEc}>SKT 알뜰폰</SelectItem>
+							<SelectItem value={mobileCarrier.ktEc}>KT 알뜰폰</SelectItem>
+							<SelectItem value={mobileCarrier.lguEc}>LGU+ 알뜰폰</SelectItem>
 						</SelectGroup>
 					</SelectContent>
 				</Select>
 
 				<BaseInput
-					ct_type="tel"
+					ct_type="number"
 					placeholder="휴대폰 번호"
 					maxLength={11}
 					className="border-none ml-1"
