a5ba24de345f434459a5327f92cc666bdd2d2ebe
ParkSinGyu <singyu13@gmail.com>
2024-09-04 17:41:04 +0900

    - FEA/develop: Create phoneVerification.tsx ---- (Why): [] ---- Done: ---- Todo:     * Implement UX functionality ---- Note: []
    -- Resolves: []
    -- See also: []

---
+++ src/components/page/auth/phoneVerifPage.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image"
+
+import BaseHeader from "@/components/atom/header/baseHeader"
+import CloseURL from "@/assets/svg/close.svg?url"
+import React from "react"
+import PhoneVerification from "@/components/organism/auth/phoneVerification"
+
+export default function phoneVerifPage() {
+	return (
+		<main className="w-screen h-screen flex flex-col hidden-scroll">
+			<BaseHeader
+				leftComponent={
+					<Image width={20} height={20} src={CloseURL} alt="close.svg"></Image>
+				}
+			></BaseHeader>
+
+			<div className="w-full h-full flex flex-col mt-[50px] mb-[90px] px-[30px] pb-8 hidden-scroll">
+				<div className="flex flex-col gap-2 mb-10 mt-3">
+					<p className="text-2xl text-sb-black-100 font-semibold">
+						본인확인을 위해
+					</p>
+					<p className="text-2xl text-sb-black-100 font-semibold">
+						인증을 진행해 주세요
+					</p>
+				</div>
+
+				<PhoneVerification></PhoneVerification>
+			</div>
+		</main>
+	)
+}
