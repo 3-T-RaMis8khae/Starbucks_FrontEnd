296bc52e79f02d8cc415e61f5ab00ba752e83a3f
ParkSinGyu <singyu13@gmail.com>
2024-09-09 21:29:21 +0900

    - FEA/develop: Add productListCateg, productListCategFilter in product-list page (init)
    ---- (Why): []
    ---- Done:
    ---- Todo:
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

--- src/app/shop/product-list/page.tsx
+++ src/app/shop/product-list/page.tsx
@@ -1,3 +1,11 @@
+import ProductListCateg from "@/components/atom/category/productListCateg"
+import ProductListCategFilter from "@/components/atom/category/productListCategFilter"
+
 export default function ProductListPage() {
-	return <section>product list</section>
+	return (
+		<section>
+			<ProductListCateg />
+			<ProductListCategFilter />
+		</section>
+	)
 }
