efcc3b7626350c40152b8478c1bffa7a10ee0bf0
ParkSinGyu <singyu13@gmail.com>
2024-09-09 20:33:13 +0900

    - FEA/develop: Create shopMainNav.tsx
    ---- (Why): []
    ---- Done:
    ---- Todo:
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

---
+++ src/components/atom/nav/shopMainNav.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import React from "react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+export interface NavItem {
+	name: string
+	href: string
+}
+
+function ShopMainNav() {
+	const pathname = usePathname()
+	console.log(pathname)
+
+	const navItems: NavItem[] = [
+		{ name: "메인", href: "/shop/main" },
+		{ name: "기획전", href: "/shop/promotion" },
+		{
+			name: "베스트",
+			href: "/shop/best"
+		},
+		{ name: "마이페이지", href: "/shop/my-page" }
+	]
+	return (
+		<nav className={`w-full flex`}>
+			{navItems.map((navItem, index) => (
+				<Link
+					key={index}
+					href={navItem.href}
+					className={`
+						flex-1 text-center py-1 px-2 text-xs font-medium text-sb-gray-100
+						${pathname === navItem.href ? "!text-sb-black-100 border-b-[2px] border-b-sb-green-100" : ""}
+					`}
+				>
+					{navItem.name}
+				</Link>
+			))}
+		</nav>
+	)
+}
+
+export default ShopMainNav
