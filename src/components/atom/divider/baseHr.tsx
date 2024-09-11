ebffaccd5bd9b442dbe8780d896fbfdbafca6cb7
ParkSinGyu <singyu13@gmail.com>
2024-09-11 14:35:48 +0900

    - MODIFY/develop: Change element of className in baseHr.tsx
    ---- (Why): []
    ---- Done:
    ---- Todo:
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

--- src/components/atom/divider/baseHr.tsx
+++ src/components/atom/divider/baseHr.tsx
@@ -6,7 +6,7 @@ function BaseHr(props: BaseHrProps) {
 	return (
 		<hr
 			{...props}
-			className={`w-full h-[1px] border-t-[1px] border-b-sb-gray-0`}
+			className={`w-full h-[1px] border-t-0 border-b-[1px] border-b-sb-gray-0 ${props.className}`}
 		/>
 	)
 }
