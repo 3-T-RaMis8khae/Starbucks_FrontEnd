b235eff6f91c238f68948559d78a03991751966c
ParkSinGyu <singyu13@gmail.com>
2024-09-11 15:17:25 +0900

    - FEA/develop: Create productCategoryTitle.tsx
    ---- (Why): []
    ---- Done:
    ---- Todo:
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

---
+++ src/components/atom/title/productCategoryTitle.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function ProductCategoryTitle() {
+	return (
+		<div className="flex flex-col gap-3">
+			<h1 className="text-2xl font-bold">상품 카테고리</h1>
+			<h2 className="text-base text-sb-black-100">
+				온라인 스토어에 오신 것을 환영합니다.
+			</h2>
+		</div>
+	)
+}
+
+export default ProductCategoryTitle
