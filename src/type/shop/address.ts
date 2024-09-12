97684cc128f32f0fcd855dbc879e038c0bb717dd
ParkSinGyu <singyu13@gmail.com>
2024-09-12 09:24:47 +0900

    - FEA/develop: Create address type
    ---- (Why): []
    ---- Done:
    ---- Todo:
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

---
+++ src/type/shop/address.ts
@@ -0,0 +1,7 @@
+export interface AddressType {
+	name: string // address nickname
+	recipient: string // recipient name
+	phone: string // recipient phone number (01042135632)
+	address: string // 우편번호, 기본주소, 상세주소
+	memo: string // memo
+}
