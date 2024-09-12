3d06fc10698c9a43ba7eaa8d596a3c2cbe9bee9f
ParkSinGyu <singyu13@gmail.com>
2024-09-12 15:17:39 +0900

    - FEA/develop: Create orderCountLinkList.tsx
    ---- (Why): []
    ---- Done:
    ---- Todo:
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

---
+++ src/components/atom/order/orderCountLinkList.tsx
@@ -0,0 +1,48 @@
+import React from "react"
+import OrderCountLink, {
+	OrderCountProps
+} from "@/components/atom/order/orderCountLink"
+import Image from "next/image"
+import CaretRightURL from "@/assets/svg/caret-right.svg?url"
+
+interface OrderCountLinkListProps {
+	items: OrderCountProps[]
+}
+
+function OrderCountLinkList({ items }: OrderCountLinkListProps) {
+	return (
+		<div>
+			<div className="flex justify-between px-6 items-baseline py-8">
+				<span className="text-xl font-bold text-sb-black-100">
+					주문/배송 현황
+				</span>
+				<span className="text-sm font-medium text-sb-gray-100">
+					최근 3개월 동안 구매한 상품
+				</span>
+			</div>
+			<div className="flex items-center justify-center gap-6 pb-14">
+				{items.map((item, index) => (
+					<>
+						<OrderCountLink
+							key={index}
+							count={item.count}
+							orderType={item.orderType}
+							orderTypeName={item.orderTypeName}
+							href={item.href}
+						/>
+						{items.length - 1 !== index && (
+							<Image
+								src={CaretRightURL}
+								alt={"caret-right.svg"}
+								width={18}
+								height={18}
+							/>
+						)}
+					</>
+				))}
+			</div>
+		</div>
+	)
+}
+
+export default OrderCountLinkList
