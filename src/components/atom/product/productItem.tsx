5b5ebfdadd412213fe26b3308a75f9b297276b6f
ParkSinGyu <singyu13@gmail.com>
2024-09-13 13:23:25 +0900

    - Modify/develop: Add condition statement for discount rate in productItem
    ---- (Why): []
    ---- Done:
    ---- Todo:
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

--- src/components/atom/product/productItem.tsx
+++ src/components/atom/product/productItem.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import Link from "next/link"
 import { ProductItemType } from "@/type/shop/product"
 import ProductThumbnail from "@/components/atom/logo/productThumbnail"
-import { toPrice } from "@/lib/productUtils"
+import { toDiscountPercent, toDiscountPrice, toPrice } from "@/lib/productUtils"
 
 interface ProductItemProps {
 	item: ProductItemType
@@ -12,17 +12,36 @@ function ProductItem({ item }: ProductItemProps) {
 	return (
 		<Link
 			href={`/shop/product-detail/${item.uuid}`}
-			className="flex flex-col flex-1 gap-1.5"
+			className="flex flex-col flex-1 gap-1"
 		>
 			<ProductThumbnail
 				imageProps={{ src: item.thumbnail, alt: `${item.name}.png` }}
 			/>
-			<span className="text-sb-black-100 font-medium text-base break-words ">
+			<span className="text-sb-black-100 font-normal text-base break-words ">
 				{item.name}
 			</span>
-			<span className="text-sb-black-100 font-bold text-base">
-				{toPrice(item.price)}원
-			</span>
+
+			{item.discountRate ? (
+				<div>
+					<span className="text-sb-gray-100 font-normal text-base line-through">
+						{toPrice(item.price)}원
+					</span>
+					<div className="flex justify-between items-center gap-1">
+						<span className="text-sb-black-100 font-bold text-base">
+							{toDiscountPrice(item.price, item.discountRate)}원
+						</span>
+						<span className="text-sb-green-100 font-bold text-base">
+							{toDiscountPercent(item.discountRate)}
+						</span>
+					</div>
+				</div>
+			) : (
+				<div className="flex justify-between items-center gap-1">
+					<span className="text-sb-black-100 font-bold text-base">
+						{toPrice(item.price)}원
+					</span>
+				</div>
+			)}
 		</Link>
 	)
 }
