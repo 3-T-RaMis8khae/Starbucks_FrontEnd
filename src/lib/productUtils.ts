ed71e045ead186b4bd86768af18aadd35c878498
ParkSinGyu <singyu13@gmail.com>
2024-09-13 13:22:28 +0900

    - FEA/develop: Add product util functions
    ---- (Why): []
    ---- Done:
    ---- Todo:
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

--- src/lib/productUtils.ts
+++ src/lib/productUtils.ts
@@ -1,3 +1,5 @@
+import _ from "lodash"
+
 export const toPrice = (price: number) => {
 	let isNegative = false
 	if (!price) {
@@ -11,4 +13,26 @@ export const toPrice = (price: number) => {
 		(isNegative ? "-" : "") +
 		_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 	)
+}
+
+export const toDiscountPercent = (discountRate: number) => {
+	return _.inRange(discountRate, 0, 1)
+		? `${discountRate * 100}%`
+		: `${discountRate}%`
+}
+
+export const toDiscountPrice = (
+	price: number,
+	discountRate: number,
+	roundPrecision = 0
+) => {
+	const discountedPrice = _.round(
+		_.inRange(discountRate, 0, 1)
+			? price * discountRate
+			: price * (discountRate / 100),
+		roundPrecision
+	)
+
+	const _price = price - discountedPrice
+	return toPrice(_price)
 }
