e854a46d282cab0bb79018e140bffc76f3faadf0
ParkSinGyu <singyu13@gmail.com>
2024-09-14 18:29:52 +0900

    - MODIFY/develop: Modify productGroupList.tsx style.
    ---- (Why): []
    ---- Done:
    ---- Todo:
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

--- src/components/molecule/product/productGroupList.tsx
+++ src/components/molecule/product/productGroupList.tsx
@@ -9,8 +9,8 @@ interface ProductGroupListProps {
 function ProductGroupList({ groupItem }: ProductGroupListProps) {
 	return (
 		<div className="flex flex-col gap-4 overflow-x-hidden w-full">
-			<span className="text-1xl font-bold">{groupItem.title}</span>
-			<div className="grid grid-rows-1 gap4 hidden-x-scroll">
+			<span className="text-2xl font-bold app-px">{groupItem.title}</span>
+			<div className="grid grid-rows-1 gap-4 hidden-x-scroll grid-flow-col app-px">
 				{groupItem.items.map((item) => (
 					<ProductItem key={item.uuid} item={item} />
 				))}
