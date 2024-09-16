0951d82c865c36d73a484a419b28f721e0c04984
ParkSinGyu <singyu13@gmail.com>
2024-09-16 22:37:08 +0900

    - FEA/develop: Create product-detail-data.ts
    ---- (Why): []
    ---- Done:
    ---- Todo:
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

---
+++ src/dummy/product-detail-data.ts
@@ -0,0 +1,46 @@
+export const productDetailImage_test =
+	"https://sitem.ssgcdn.com/32/41/57/item/1000614574132_i1_750.jpg"
+// "https://sitem.ssgcdn.com/97/49/57/item/1000614574997_i1_750.jpg"
+
+export const productDetailData_tumbler1 = {
+	uuid: "1",
+	name: "[스타벅스] 애니버서리 켄처 텀블러 414ml",
+	description:
+		"귀여운 동물들의 일러스트가 담긴 473ml 용량의 찬 음료 전용 글라스 콜드컵입니다.",
+	price: 32000,
+	thumbnails: [
+		"https://sitem.ssgcdn.com/32/41/57/item/1000614574132_i1_750.jpg"
+	],
+	images: [
+		{
+			uuid: "tumbler_kenthur_0",
+			url: "https://prod-starbucks-product-details.s3.ap-northeast-2.amazonaws.com/figma/ssg/common/logo_cp.png"
+		},
+		{
+			uuid: "tumbler_kenthur_1",
+			url: "https://prod-starbucks-product-details.s3.ap-northeast-2.amazonaws.com/figma/ssg/product/11156009.png"
+		}
+	]
+}
+
+export const productDetailData_tumbler2 = {
+	uuid: "1",
+	name: "[스타벅스] 애니버서리 켄처 텀블러 414ml",
+	description:
+		"귀여운 동물들의 일러스트가 담긴 473ml 용량의 찬 음료 전용 글라스 콜드컵입니다.",
+	price: 32000,
+	thumbnails: [
+		"https://sitem.ssgcdn.com/32/41/57/item/1000614574132_i1_750.jpg",
+		"https://sitem.ssgcdn.com/32/41/57/item/1000614574132_i1_750.jpg"
+	],
+	images: [
+		{
+			uuid: "tumbler_kenthur_0",
+			url: "https://prod-starbucks-product-details.s3.ap-northeast-2.amazonaws.com/figma/ssg/common/logo_cp.png"
+		},
+		{
+			uuid: "tumbler_kenthur_1",
+			url: "https://prod-starbucks-product-details.s3.ap-northeast-2.amazonaws.com/figma/ssg/product/11156009.png"
+		}
+	]
+}
