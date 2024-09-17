4fcb00021b2d3e61fbd94eb5c00d47ba152266e8
ParkSinGyu <singyu13@gmail.com>
2024-09-17 22:58:23 +0900

    - TEST/develop:  Add product-review-data.ts
    ---- (Why): []
    ---- Done:
    ---- Todo:
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

---
+++ src/dummy/product-review-data.ts
@@ -0,0 +1,75 @@
+import { ProductReviewType } from "@/type/shop/review"
+
+export const productReviewData: Array<ProductReviewType> = [
+	{
+		id: "1",
+		userName: "김개똥",
+		score: 4.5,
+		productId: "1",
+		content: "좋아요",
+		createdAt: "2021-08-01",
+		images: [
+			"https://simg.ssgcdn.com/trans.ssg?src=/uphoto/202409/20240904074637_1225083845_0_1.jpg&w=500&h=500&autoOrient=true&t=630bb725deeb21ee6b6d4e1ac94e3386cc047d9b",
+			"https://simg.ssgcdn.com/trans.ssg?src=/uphoto/202409/20240902144154_1224984910_0_1.jpg&w=500&h=500&autoOrient=true&t=ca6b39527eb127c1981e4855f157fd5ec8752286"
+		]
+	},
+	{
+		id: "2",
+		userName: "박똥개",
+		score: 3.5,
+		productId: "1",
+		content: "별로에요",
+		createdAt: "2021-08-02",
+		images: [
+			"https://simg.ssgcdn.com/trans.ssg?src=/uphoto/202409/20240904074637_1225083845_0_1.jpg&w=500&h=500&autoOrient=true&t=630bb725deeb21ee6b6d4e1ac94e3386cc047d9b"
+		]
+	},
+	{
+		id: "3",
+		userName: "홍길동",
+		score: 5,
+		productId: "1",
+		content: "좋아요",
+		createdAt: "2021-08-03"
+	},
+	{
+		id: "4",
+		userName: "김철수",
+		score: 4,
+		productId: "1",
+		content: "좋아요",
+		createdAt: "2021-08-04"
+	},
+	{
+		id: "5",
+		userName: "김영희",
+		score: 4.5,
+		productId: "1",
+		content: "좋아요",
+		createdAt: "2021-08-05"
+	},
+	{
+		id: "6",
+		userName: "이영희",
+		score: 3.5,
+		productId: "1",
+		content: "별로에요",
+		createdAt: "2021-08-06"
+	},
+	{
+		id: "7",
+		userName: "박철수",
+		score: 5,
+		productId: "1",
+		content: "좋아요",
+		createdAt: "2021-08-07"
+	},
+	{
+		id: "8",
+		userName: "이철수",
+		score: 4,
+		productId: "1",
+		content: "좋아요",
+		createdAt: "2021-08-07"
+	}
+]
