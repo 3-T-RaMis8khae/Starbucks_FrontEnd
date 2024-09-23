ddca243f4a328fd061fc44dc323de84a36617d3e
ParkSinGyu <singyu13@gmail.com>
2024-09-23 15:37:33 +0900

    - FEA/develop: Create Config for route.ts
    ---- (Why): []
    ---- Done:
    ---- Todo:
    
    ---- Note: []
    
    -- Resolves: []
    -- See also: []

---
+++ src/config/route.ts
@@ -0,0 +1,9 @@
+export const routes = {
+	signIn: "/login",
+	signup: "/signup",
+	shop_main: "/shop/main",
+	shop_best: "/shop/best",
+	shop_productList: "/shop/product-list",
+	shop_productDetail: "/shop/product-detail",
+	shop_productCategory: "/shop/product-category"
+}
